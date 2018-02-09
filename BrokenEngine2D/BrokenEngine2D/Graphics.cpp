fc01f90733905c78e9c03c75bf4e3d581db975c8
Jesper Bergström <jesper.b96@gmail.com>
2018-02-09 12:35:14 +0100

    Implemented DrawLine()

--- BrokenEngine2D/BrokenEngine2D/Graphics.cpp
+++ BrokenEngine2D/BrokenEngine2D/Graphics.cpp
@@ -1,4 +1,6 @@
 #include "Graphics.hpp"
+#include <math.h>
+#include <stdio.h>
 
 Graphics::Graphics()
 {
@@ -17,6 +19,55 @@ void Graphics::Draw(float x, float y)
 
 void Graphics::DrawLine(float x1, float y1, float x2, float y2)
 {
+	// Swap points if x1 is to the right of x2.
+	if (x1 > x2) {
+		float tempx = x2;
+		x2 = x1;
+		x1 = tempx;
+		float tempy = y2;
+		y2 = y1;
+		y1 = tempy;
+	}
+
+	float k; // coefficient value
+	if (x2 - x1 == 0) {
+		k = 10000; // Instead of infinity.
+		if (y2 - y1 < 0) {
+			k = k * -1;
+		}
+	}
+	else {
+		k = (y2 - y1) / (x2 - x1);
+	}
+	
+	float kx;
+	float ky;
+
+	// Calculating how much x and y will increase each iteration of drawing the line.
+	if (k < 0) {
+		kx = 1 / (-1 + k) * -1;
+		ky = k / (-1 + k) * -1;
+	}
+	else {
+		kx = 1 / (1 + k);
+		ky = k / (1 + k);
+	}
+
+	float totLength = std::sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)); // Total length of the line.
+	float length = 0;
+	float increment = std::sqrt(kx * kx + ky * ky); // Line growth after every iteration of drawing it.
+
+	// Draw loop.
+	for (int i = 0; length <= totLength; i++) {
+		float x = x1 + kx * i;
+		float y = y1 + ky * i;
+		int a = m_screenWidth * int(y + 0.5) + int(x + 0.5);
+
+		m_bufScreen[a].Char.UnicodeChar = 0x2588;
+		m_bufScreen[a].Attributes = 0x000F;
+
+		length += increment;
+	}
 }
 
 void Graphics::DrawString(float x, float y, std::string str)
--- BrokenEngine2D/TestGame/Main.cpp
+++ BrokenEngine2D/TestGame/Main.cpp
@@ -14,8 +14,17 @@ class testGame : public BrokenEngine2D
 
 	virtual bool onRender(Graphics g)
 	{
-		g.Draw(30.3, 50.6);
+		g.Draw(18, 18);
 		g.Fill(50, 40, 10, 10);
+		g.DrawLine(20, 20, 30, 10);
+		g.DrawLine(22, 22, 30, 40);
+		g.DrawLine(10, 50, 30, 50);
+		g.DrawLine(30, 55, 10, 55);
+		g.DrawLine(34, 10, 24, 20);
+		g.DrawLine(40, 10, 40, 20);
+		g.DrawLine(50, 20, 50, 10);
+		g.Draw(10, 48);
+		g.Draw(30, 48);
 		return true;
 	}
 };
